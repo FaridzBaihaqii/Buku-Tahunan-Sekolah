54996df0727cbb6801e9e8673a23be84c406e26b
FaridzBaihaqii <muhammadfaridzbaihaqii@gmail.com>
2024-01-11 07:20:54 +0700

    perubahan

--- database/migrations/2023_11_09_012353_create_detail_siswas_table.php
+++ database/migrations/2023_11_09_012353_create_detail_siswas_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('detail_siswas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            // $table->id();
+            // $table->timestamps();
         });
     }
 
